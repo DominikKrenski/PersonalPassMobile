bd002053dedaec25505dba48e58ef6806235eec6
Dominik Kreński <dominik.krenski@gmail.com>
2022-01-24 14:11:15 +0100

    MainActivity moved to activities package

--- app/src/main/java/org/dominik/pass/activities/MainActivity.java
+++ app/src/main/java/org/dominik/pass/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package org.dominik.pass;
+package org.dominik.pass.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import org.dominik.pass.R;
 
 public class MainActivity extends AppCompatActivity {
 
