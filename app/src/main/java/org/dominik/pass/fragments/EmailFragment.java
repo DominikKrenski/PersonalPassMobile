a1d6fa9f4880a54132172e7691b6324b0af4ed4b
Dominik Kreński <dominik.krenski@gmail.com>
2022-01-26 22:00:09 +0100

    EmailFragment updated to handle api calls

--- app/src/main/java/org/dominik/pass/fragments/EmailFragment.java
+++ app/src/main/java/org/dominik/pass/fragments/EmailFragment.java
@@ -5,20 +5,39 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputLayout;
 
 import org.dominik.pass.R;
+import org.dominik.pass.errors.ApiError;
+import org.dominik.pass.http.client.ApiClient;
+import org.dominik.pass.http.dto.AuthDTO;
+import org.dominik.pass.http.dto.EmailDTO;
+import org.dominik.pass.http.service.PassService;
+import org.dominik.pass.http.utils.ErrorConverter;
 import org.dominik.pass.utils.Validator;
 
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.util.Locale;
 import java.util.Objects;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class EmailFragment extends Fragment {
+  private static final String TAG = "EMAIL_FRAGMENT";
+
+  private View view;
   private TextInputLayout inputLayout;
+  private PassService passService;
 
   public EmailFragment() { }
 
@@ -31,11 +50,13 @@ public class EmailFragment extends Fragment {
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
 
-    View view = inflater.inflate(R.layout.fragment_email, container, false);
+    view = inflater.inflate(R.layout.fragment_email, container, false);
 
     MaterialButton signupBtn = view.findViewById(R.id.email_signup_button);
     MaterialButton signinBtn = view.findViewById(R.id.email_signin_button);
     inputLayout = view.findViewById(R.id.email_input);
+
+    passService = ApiClient.getInstance().create(PassService.class);
 
     Validator validator = Validator.getInstance();
 
@@ -57,9 +78,7 @@ public class EmailFragment extends Fragment {
         return;
       }
 
-
-      //TODO implement method to check if email is already in use
-      //TODO implement fragment swap if everything is all right
+      getAuthInfo(email);
 
     });
 
@@ -76,5 +95,44 @@ public class EmailFragment extends Fragment {
 
     String email = Objects.requireNonNull(inputLayout.getEditText()).getText().toString();
     outState.putString("email", email);
+  }
+
+  private void getAuthInfo(String email) {
+    Call<AuthDTO> call = passService.getAuthInfo(new EmailDTO(email.toLowerCase(Locale.ROOT)));
+    call.enqueue(new Callback<AuthDTO>() {
+      @Override
+      public void onResponse(Call<AuthDTO> call, Response<AuthDTO> response) {
+        if (response.isSuccessful()) {
+          if (response.body() != null) {
+            // salt has been found, it means that account already exists
+            Log.d(TAG, response.body().toString());
+            Toast.makeText(view.getContext(), view.getResources().getString(R.string.email_in_use), Toast.LENGTH_LONG).show();
+          }
+        } else {
+          if (response.code() == 404) {
+            // salt has not been found, it means that given email can be used
+            //TODO go to the next screen
+          } else {
+            ApiError apiError = ErrorConverter.getInstance().parseError(response);
+            Log.d(TAG, apiError.toString());
+          }
+        }
+      }
+
+      @Override
+      public void onFailure(Call<AuthDTO> call, Throwable error) {
+        if (error instanceof SocketTimeoutException) {
+          Log.d(TAG, "CONNECTION TIMEOUT");
+        } else if (error instanceof IOException) {
+          Log.d(TAG, "TIMEOUT");
+        } else {
+          if (call.isCanceled()) {
+            Log.d(TAG, "REQUEST WAS CANCELLED BY THE USER");
+          } else {
+            Log.d(TAG, "GENERIC ERROR");
+          }
+        }
+      }
+    });
   }
 }
