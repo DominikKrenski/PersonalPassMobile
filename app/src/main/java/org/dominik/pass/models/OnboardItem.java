6c1e725f14adc0e465985034f98e97da676a41cb
Dominik Kreński <dominik.krenski@gmail.com>
2022-01-26 09:03:20 +0100

    OnboardItem refactored to not use Lombok

--- app/src/main/java/org/dominik/pass/models/OnboardItem.java
+++ app/src/main/java/org/dominik/pass/models/OnboardItem.java
@@ -1,16 +1,25 @@
 package org.dominik.pass.models;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@RequiredArgsConstructor
-@Getter
-@Setter
-@ToString
 public final class OnboardItem {
   final int imageId;
   final String title;
   final String message;
+
+  public OnboardItem(int imageId, String title, String message) {
+    this.imageId = imageId;
+    this.title = title;
+    this.message = message;
+  }
+
+  public int getImageId() {
+    return imageId;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getMessage() {
+    return message;
+  }
 }
