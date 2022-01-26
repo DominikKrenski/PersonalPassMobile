b85469d5cbf4c26bbdb672dc9d53774e569e4a07
Dominik Kreński <dominik.krenski@gmail.com>
2022-01-26 21:59:35 +0100

    Converter for api errors added

---
+++ app/src/main/java/org/dominik/pass/http/utils/ErrorConverter.java
@@ -0,0 +1,44 @@
+package org.dominik.pass.http.utils;
+
+import org.dominik.pass.errors.ApiError;
+import org.dominik.pass.http.client.ApiClient;
+
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.time.Instant;
+
+import okhttp3.ResponseBody;
+import retrofit2.Converter;
+import retrofit2.Response;
+
+public final class ErrorConverter {
+  private static ErrorConverter INSTANCE;
+
+  private ErrorConverter() {}
+
+  public static ErrorConverter getInstance() {
+    if (INSTANCE == null) {
+      INSTANCE = new ErrorConverter();
+    }
+
+    return INSTANCE;
+  }
+
+  public ApiError parseError(Response<?> response) {
+    Converter<ResponseBody, ApiError> converter = ApiClient.getInstance().responseBodyConverter(ApiError.class, new Annotation[0]);
+
+    ApiError apiError = null;
+
+    try {
+      assert response.errorBody() != null;
+      apiError = converter.convert(response.errorBody());
+    } catch (IOException ex) {
+      apiError = new ApiError();
+      apiError.setStatus("Internal Server Error");
+      apiError.setMessage("Error could not be deserialized");
+      apiError.setTimestamp(Instant.now());
+    }
+
+    return apiError;
+  }
+}
