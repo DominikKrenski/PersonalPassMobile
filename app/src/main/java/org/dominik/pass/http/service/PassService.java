40f195a24dfca53393f9cadce838c55abc5ee4dd
Dominik Kreński <dominik.krenski@gmail.com>
2022-02-02 20:00:57 +0100

    PassService updated

--- app/src/main/java/org/dominik/pass/http/service/PassService.java
+++ app/src/main/java/org/dominik/pass/http/service/PassService.java
@@ -2,14 +2,21 @@ package org.dominik.pass.http.service;
 
 import org.dominik.pass.http.dto.AuthDTO;
 import org.dominik.pass.http.dto.EmailDTO;
+import org.dominik.pass.http.dto.LoginDataDTO;
+import org.dominik.pass.http.dto.RegistrationDataDTO;
 
-import retrofit2.Call;
+import io.reactivex.rxjava3.core.Observable;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface PassService {
 
   @POST("auth/salt")
-  Call<AuthDTO> getAuthInfo(@Body EmailDTO emailDTO);
+  Observable<AuthDTO> getAuthInfo(@Body EmailDTO emailDTO);
 
+  @POST("auth/signup")
+  Observable<AuthDTO> signup(@Body RegistrationDataDTO data);
+
+  @POST("auth/signin")
+  Observable<AuthDTO> signin(@Body LoginDataDTO data);
 }
