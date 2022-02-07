f04a965d0cec5e13ef912185a9cfe4b2fa4641ed
Dominik Kreński <dominik.krenski@gmail.com>
2022-02-07 18:20:04 +0100

    RequestInterceptor finished

--- app/src/main/java/org/dominik/pass/http/interceptors/RequestInterceptor.java
+++ app/src/main/java/org/dominik/pass/http/interceptors/RequestInterceptor.java
@@ -1,37 +1,48 @@
 package org.dominik.pass.http.interceptors;
 
-import android.util.Log;
-
 import androidx.annotation.NonNull;
 
+import org.dominik.pass.models.AccessDataRaw;
+import org.dominik.pass.services.AccessService;
+
 import java.io.IOException;
-import java.util.Objects;
 
 import okhttp3.Interceptor;
 import okhttp3.Request;
-import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public final class RequestInterceptor implements Interceptor {
+  private static final String AUTHORIZATION_HEADER = "Authorization";
+  private static final String BEARER = "Bearer";
+  private static final String REFRESH_URL = "/api/auth/refresh";
+  private static final String SIGNUP_URL = "/api/auth/signup";
+  private static final String SIGNIN_URL = "/api/auth/signin";
+  private static final String SALT_URL = "/api/auth/salt";
+  private static final String HINT_URL = "/api/accounts/hint";
+
   private static final String TAG = "REQUEST_INTERCEPTOR";
+
+  private AccessDataRaw accessData;
+
+  public RequestInterceptor(AccessService accessService) {
+    accessService.getAccessData().subscribe(data -> accessData = data);
+  }
 
   @NonNull
   @Override
   public Response intercept(@NonNull Chain chain) throws IOException {
     Request request = chain.request();
-    RequestBody body = request.body();
+    String path = request.url().encodedPath();
 
+    if (path.equals(REFRESH_URL)) {
+      request = request.newBuilder().header(AUTHORIZATION_HEADER, BEARER + " " + accessData.getRefreshToken()).build();
+      return chain.proceed(request);
+    }
 
-    Log.d(TAG, Objects.requireNonNull(Objects.requireNonNull(request.body()).contentType()).toString());
+    if (!path.equals(SIGNUP_URL) && !path.equals(SIGNIN_URL) && !path.equals(SALT_URL) && !path.equals(HINT_URL)) {
+      request = request.newBuilder().header(AUTHORIZATION_HEADER, BEARER + " " + accessData.getAccessToken()).build();
+    }
 
-    request = request
-      .newBuilder()
-      .removeHeader("Content-Type")
-      .addHeader("Content-Type", "application/json")
-      .method(request.method(), request.body())
-      .build();
-
-    Log.d(TAG, request.header("Content-Type"));
     return chain.proceed(request);
   }
 }
