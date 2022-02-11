717f7492418bb1d48939f8e840904915ebac6334
Dominik Kreński <dominik.krenski@gmail.com>
2022-02-11 12:11:22 +0100

    DataViewModel updated to fetch all data from server

--- app/src/main/java/org/dominik/pass/viewmodels/DataViewModel.java
+++ app/src/main/java/org/dominik/pass/viewmodels/DataViewModel.java
@@ -2,16 +2,25 @@ package org.dominik.pass.viewmodels;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import org.dominik.pass.errors.ErrorWrapper;
 import org.dominik.pass.http.dto.DataDTO;
 import org.dominik.pass.http.repositories.PassRepository;
+import org.dominik.pass.models.AccessDataRaw;
+import org.dominik.pass.services.AccessService;
 import org.dominik.pass.services.EncryptionService;
 
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.List;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.disposables.Disposable;
@@ -22,31 +31,45 @@ public final class DataViewModel extends ViewModel {
 
   private PassRepository passRepository;
   private EncryptionService encryptionService;
+  private AccessService accessService;
+  private AccessDataRaw accessData;
 
   private Disposable allDataDisposable;
+  private Disposable accessDataDisposable;
 
   private final MutableLiveData<List<DataDTO>> allData = new MutableLiveData<>();
 
-  public void init(PassRepository passRepository, EncryptionService encryptionService) {
+  public void init(PassRepository passRepository, EncryptionService encryptionService, AccessService accessService) {
     this.passRepository = passRepository;
     this.encryptionService = encryptionService;
+    this.accessService = accessService;
+    accessDataDisposable = accessService.getAccessData().subscribe(data -> accessData = data);
   }
 
-  public void getAllData() {
+  public void fetchAllData() {
     allDataDisposable = passRepository
       .getAllData()
       .subscribeOn(Schedulers.io())
       .observeOn(AndroidSchedulers.mainThread())
       .subscribe(res -> {
+        res.forEach(el -> {
+          try {
+            el.setEntry(encryptionService.decryptData(el.getEntry(), accessData.getDerivationKey()));
+          } catch (NoSuchPaddingException | NoSuchAlgorithmException | InvalidAlgorithmParameterException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+            e.printStackTrace();
+          }
+        });
+
         allData.setValue(res);
-        Log.d(TAG, res.toString());
-      },
-        err -> Log.d(TAG, Arrays.toString(err.getStackTrace())));
+      }, err-> Log.d(TAG, Arrays.toString(err.getStackTrace())));
   }
+
+  public LiveData<List<DataDTO>> getAllData() { return allData; }
 
   @Override
   protected void onCleared() {
     allDataDisposable.dispose();
+    accessDataDisposable.dispose();
     super.onCleared();
   }
 }
